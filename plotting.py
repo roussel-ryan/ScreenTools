0374df23e3a9c72727b12bc081980306933fc0c5
U-awa3\awa <awa@awa3.hep.anl.gov>
2018-12-10 10:55:46 -0600

    added doc strings to plotting.py

--- plotting.py
+++ plotting.py
@@ -14,6 +14,23 @@ def check_axes(axes):
         return ax
 
 def plot_screen(filename,dataset = '/img',image_number=0,ax=None,scaled=False):
+    '''
+    Plot screen image for a given frame
+    
+    Inputs
+    ------
+    filename         h5 file with screen information
+    dataset          specify what type of image to plot (either /img or /raw
+    image_number     frame number for image
+    ax               matplotlib.axes object
+    scaled           if True, scale image using given pixel scale
+
+    Outputs
+    -------
+    matplotlib.axes object with image plot
+
+    '''
+
     ax = check_axes(ax)
 
     logging.info('plotting file {}, image # {}'.format(filename,image_number))
@@ -33,6 +50,22 @@ def plot_screen(filename,dataset = '/img',image_number=0,ax=None,scaled=False):
     return ax
 
 def plot_current(filename,image_number=0,ax=None):
+    '''
+    plot current data from ICT for given frame
+    
+    Inputs
+    ------
+    filename        h5 file with screen information
+    image_number    frame number
+    ax              matplotlib.axes object for plotting
+
+    Output
+    ------
+    matplotlib.axes object with current plot
+
+    '''
+
+
     ax = check_axes(ax)
     with h5py.File(filename,'r') as f:
         data = f['/{}/current'.format(image_number)][:]
@@ -41,6 +74,18 @@ def plot_current(filename,image_number=0,ax=None):
         
     
 def overlay_lineout(filename,ax,image_number=0,axis=0):
+    '''
+    overlay a projection (lineout) onto a image plot
+
+    Inputs
+    ------
+    filename      h5 filename with screen info
+    ax            matplotlib.axes object with screen image plotted
+    image_number  frame number for plotting
+    axis          0 for horizontal lineout, 1 for vertical axis lineout
+
+    '''
+    
     lineout = analysis.get_lineout(filename,image_number,axis)
     xlim = ax.get_xlim()
     ylim = ax.get_ylim()
